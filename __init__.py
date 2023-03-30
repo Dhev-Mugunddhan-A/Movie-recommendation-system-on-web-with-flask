ed451c9a86a315f9a06103e94331cb0f3e49b8cf
DHEV MUGUNDDHAN A <104367907+Dhev-Mugunddhan-A@users.noreply.github.com>
2023-03-30 10:06:03 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 19:38:20 2023
+
+@author: WELCOME
+"""
+
+import os
+import dill
+import pandas as pd
+from flask import Flask,render_template,request
+import psycopg2 as sql
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    conn=sql.connect(database='test1',user='postgres',password='root',host='127.0.0.1',port='5432')
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def main():
+        return render_template("login.html",msg='')
+
+    @app.route('/login',methods=['POST'])
+    def login():
+        ulist= [i for i in (request.form.values())]
+        uname = str(ulist[0])
+        password = str(ulist[1])
+        q='select username,password from logindet where username=\'{}\''.format(uname)
+        df = pd.read_sql_query(q, conn)
+        if(not(df.empty)):
+            if(df.iloc[0,0]==uname and df.iloc[0,1]==password):
+                return render_template('index.html')
+        return render_template('login.html',msg='Invalid credentials')
+        
+    @app.route("/predict", methods=['post'])
+    def pred():
+        model = dill.load(open('model.pkl','rb'))
+        features = [i for i in (request.form.values())]
+        pred = model.content_recommender(str(features[0]))
+        print(pred)
+        return render_template("success.html",data=pred)
+    @app.route("/exit", methods=['POST'])
+    def exit1():
+        return render_template("login.html",msg='')
+    @app.route('/home',methods=['POST'])
+    def home():
+        return render_template('index.html')
+    return app
